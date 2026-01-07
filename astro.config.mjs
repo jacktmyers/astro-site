d5372d5d094edd3856aaaef11810925f2fa904b5
Jack Myers <myers395@purdue.edu>
2026-01-07 18:02:28 -0500

    custom domain swap

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: 'https://jacktmyers.github.io',
-  base: '/astro-site'
+  site: 'https://jacktmyers.com',
 });
